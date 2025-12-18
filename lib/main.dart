4f158ea27877bd3a254ca9a16f239d4251738c5e
Shady Mohamed <shadymohamedabdo@gmail.com>
2025-12-18 13:44:59 +0200

    before handling design and cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import 'home_screen.dart';
+import 'data/screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // تهيئة قاعدة البيانات للـ Windows/Desktop
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+  runApp(const CoffeePOSApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeePOSApp extends StatelessWidget {
+  const CoffeePOSApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'coffee_pos',
-      home: HomeScreen(),
+      title: 'Coffee POS',
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
